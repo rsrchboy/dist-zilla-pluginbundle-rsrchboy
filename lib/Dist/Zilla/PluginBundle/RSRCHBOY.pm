451a9d604a8571f0874e33d2e06ffaff7425996f
Chris Weyl <cweyl@alumni.drew.edu>
2011-03-31 15:23:49 -0700

    initial commit

---
+++ lib/Dist/Zilla/PluginBundle/RSRCHBOY.pm
@@ -0,0 +1,10 @@
+package Dist::Zilla::PluginBundle::RSRCHBOY;
+
+# ABSTRACT: The great new Dist::Zilla::PluginBundle::RSRCHBOY!
+
+use Moose;
+use namespace::autoclean;
+use common::sense;
+
+
+__PACKAGE__->meta->make_immutable;
---
+++ t/00-load.t
@@ -0,0 +1,7 @@
+#!/usr/bin/env perl
+
+use Test::More tests => 1;
+
+BEGIN { use_ok '' }
+
+diag("Testing Dist-Zilla-PluginBundle-RSRCHBOY $Dist::Zilla::Plugin::lib/Dist/Zilla/PluginBundle/RSRCHBOY.pm::VERSION, Perl $], $^X");
